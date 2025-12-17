d8e38227d1ce8e49920ac1be5ccbddb70b68ec2e
mikhailkravchenko <pirog11@ya.ru>
2025-12-17 15:09:07 +0300

    Disable NAT

--- terraform/modules/tf-yc-instance/main.tf
+++ terraform/modules/tf-yc-instance/main.tf
@@ -22,7 +22,7 @@ resource "yandex_compute_instance" "vm" {
 
   network_interface {
     subnet_id          = var.subnet_id
-    nat                = true
+    nat                = false
     security_group_ids = var.security_group_ids
   }
 
